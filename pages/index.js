2251f74a3e87d441d43ed959ce50fcd536b52a01
rima1881 <110826233+rima1881@users.noreply.github.com>
2022-08-09 03:16:40 -0700

    modified:   pages/index.js 	modified:   components/Card/Card.module.css 	modified:   components/Main/Main.jsx 	modified:   components/Main/Main.module.css 	modified:   data.js 	components/Footer/ 	components/Wallpaper/ 	public/images/alaska.jpg 	public/images/anguilla.jpg 	public/images/antarctica.jpg 	public/images/bahamas.jpg 	public/images/wallpaper.jpg

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
+import Footer from '../components/Footer/Footer'
 import Header from '../components/Header/Header'
 import Main from '../components/Main/Main'
+import Wallpaper from '../components/Wallpaper/Wallpaper'
 
 export default function Home() {
   return (
@@ -10,10 +12,13 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
         <script src="https://kit.fontawesome.com/9e669c9765.js" crossorigin="anonymous"></script>
-
       </Head>
       <Header />
+      
+      <Wallpaper />
       <Main />
+
+      <Footer />
     </>
   )
 }
